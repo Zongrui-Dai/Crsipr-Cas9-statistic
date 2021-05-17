cf0e091076c4130e54e5b1babdeef226a084a8c4
Zongrui Dai <84027429+Zongrui-Dai@users.noreply.github.com>
2021-05-17 22:29:43 +0800

    Add files via upload

---
+++ Rcode/Genome/NGN statistic by R.R
@@ -0,0 +1,28 @@
+############################################
+# The statistic figure for the NGN detection
+# By Zongrui Dai: dzr17723980497@gmail.com
+############################################
+library(ggplot2)
+library(reshape2)
+setwd('D:/test')
+d<-read.csv('NGN.csv')
+chr19<-d[1:19,]
+ID<-c('chr1','chr2','chr3','chr4','chr5','chr6',
+      'chr7','chr8','chr9','chr10','chr11','chr12',
+      'chr13','chr14','chr15','chr16','chr17','chr18',
+      'chr19')
+chr19$ID<-as.factor(ID)
+
+ggplot(chr19,aes(x=ID,y=NGN))+
+  geom_bar(stat = 'identity',aes(fill = ID),width = 0.9) +
+  theme(text=element_text(family="Songti SC",size=12,face = "bold"), #设置文字的字体字号（设置字体是为了确保汉字可以显示，字号和加粗请随意）
+        axis.text.x = element_text(size=10,angle=45))+
+  ggtitle('The frequency of NGN')
+
+chr19$per<-chr19$NGN/chr19$Length
+ggplot(chr19,aes(x=ID,y=per))+
+  geom_bar(stat = 'identity',aes(fill = ID),width = 0.9) +
+  theme(text=element_text(family="Songti SC",size=12,face = "bold"), #设置文字的字体字号（设置字体是为了确保汉字可以显示，字号和加粗请随意）
+        axis.text.x = element_text(size=10,angle=45))+
+  ggtitle('The percentage of NGN')
+
